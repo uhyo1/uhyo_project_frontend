b67817abb78a78575c6bdcd00e216ec39c495c8e
uhyo <asw7@hotmail.co.jp>
2026-04-26 01:28:52 +0900

    bugFix：画面リンクを修正

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <nav>
+    <router-link to="/tasklist">Task List</router-link>
+    <router-link to="/expired">Expired List</router-link>
+    <router-link to="/select1">Select 1</router-link>
+  </nav>
+
   <router-view />
 </template>
 
